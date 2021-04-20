f8ad6adf6080c77aa943514c637122194219518e
evigilatus <evigilatus@abv.bg>
2021-04-20 23:33:12 +0300

    Add S-BERT experiment
     Date:      Tue Apr 20 23:33:12 2021 +0300

--- baselines/bm25.py
+++ baselines/bm25.py
@@ -1,27 +1,23 @@
-import pdb
-import json
-import pandas as pd
-import random
-import numpy as np
-import logging
 import argparse
+import json
+import logging
 import os
-from os.path import join, dirname, basename, exists
-from tqdm import tqdm
-from glob import glob
 import random
-from elasticsearch import Elasticsearch
-
 import sys
+from glob import glob
+from os.path import join, dirname, basename, exists
+
+import pandas as pd
+
 sys.path.append('.')
 
 from scorer.main import evaluate
 from gensim import corpora
 from gensim.summarization import bm25
 
-
 random.seed(0)
 ROOT_DIR = dirname(dirname(__file__))
+
 
 def load_vclaims(dir):
     vclaims_fp = glob(f'{dir}/*.json')
@@ -35,12 +31,14 @@ def load_vclaims(dir):
         vclaims_list.append(vclaim)
     return vclaims, vclaims_list
 
+
 def get_bm25(docs):
     texts = [doc.split() for doc in docs]  # you can do preprocessing as removing stopwords
     dictionary = corpora.Dictionary(texts)
     corpus = [dictionary.doc2bow(text) for text in texts]
     bm25_obj = bm25.BM25(corpus)
     return [bm25_obj, dictionary]
+
 
 def get_score(iclaim, bm25_obj, dictionary, vclaims_list, index, search_keys, size=10000):
     query_doc = dictionary.doc2bow(iclaim.split())
@@ -50,6 +48,7 @@ def get_score(iclaim, bm25_obj, dictionary, vclaims_list, index, search_keys, si
     for best_doc in best_docs:
         score.append((vclaims_list[best_doc]['vclaim_id'], scores[best_doc]))
     return score
+
 
 def get_scores(iclaims, bm25_obj, dictionary, vclaims_list, index, search_keys, size):
     iclaims_count, vclaims_count = len(iclaims), len(vclaims_list)
@@ -61,12 +60,14 @@ def get_scores(iclaims, bm25_obj, dictionary, vclaims_list, index, search_keys, 
         scores[iclaim_id] = score
     return scores
 
+
 def format_scores(scores):
     output_string = ''
     for iclaim_id in scores:
         for i, (vclaim_id, score) in enumerate(scores[iclaim_id]):
-            output_string += f"{iclaim_id}\tQ0\t{vclaim_id}\t{i+1}\t{score}\telasic\n"
+            output_string += f"{iclaim_id}\tQ0\t{vclaim_id}\t{i + 1}\t{score}\telasic\n"
     return output_string
+
 
 def run_baselines(args):
     if not exists('baselines/data'):
@@ -80,21 +81,23 @@ def run_baselines(args):
     for iclaim_id in wanted_iclaim_ids:
         iclaim = all_iclaims.iclaim[all_iclaims.iclaim_id == iclaim_id].iloc[0]
         iclaims.append((iclaim_id, iclaim))
-    
+
     index = f"{args.subtask}-{args.lang}"
 
     # options are title, vclaim, text
     bm25_obj, dictionary = get_bm25([vclaim['text'] for vclaim in vclaims_list])
     scores = get_scores(iclaims, bm25_obj, dictionary, vclaims_list, index, search_keys=args.keys, size=args.size)
-    ngram_baseline_fpath = join(ROOT_DIR, f'baselines/data/subtask_{args.subtask}_bm25_{args.lang}_{basename(args.dev_file_path)}')
+    ngram_baseline_fpath = join(ROOT_DIR,
+                                f'baselines/data/subtask_{args.subtask}_bm25_{args.lang}_{basename(args.dev_file_path)}')
     formatted_scores = format_scores(scores)
     with open(ngram_baseline_fpath, 'w') as f:
         f.write(formatted_scores)
     maps, mrr, precisions = evaluate(args.dev_file_path, ngram_baseline_fpath)
-    logging.info(f"Elasticsearch Baseline for Subtask-{args.subtask}--{args.lang}")
+    logging.info(f"BM-25 Baseline for Subtask-{args.subtask}--{args.lang}")
     logging.info(f'All MAP scores on threshold from [1, 3, 5, 10, 20, 50, 1000]. {maps}')
     logging.info(f'MRR score {mrr}')
     logging.info(f'All P scores on threshold from [1, 3, 5, 10, 20, 50, 1000]. {precisions}')
+
 
 # python baselines/bm25.py --train-file-path=baselines/v1/train.tsv --dev-file-path=baselines/v1/train.tsv --vclaims-dir-path=baselines/politifact-vclaims --iclaims-file-path=baselines/v1/iclaims.queries --subtask=2b --lang=english
 if __name__ == '__main__':
--- baselines/sbert.py
+++ baselines/sbert.py
@@ -1,4 +1,3 @@
-import json
 import argparse
 import json
 import logging
@@ -7,18 +6,18 @@ import random
 import sys
 from glob import glob
 from os.path import join, dirname, basename, exists
-
+from scorer.main import evaluate
+from sentence_transformers import SentenceTransformer, util
+from nltk.tokenize import sent_tokenize
 import pandas as pd
 
 sys.path.append('.')
 
-from scorer.main import evaluate
-from gensim import corpora
-from gensim.summarization import bm25
-
 
 random.seed(0)
 ROOT_DIR = dirname(dirname(__file__))
+sbert = SentenceTransformer('paraphrase-distilroberta-base-v1')
+
 
 def load_vclaims(dir):
     vclaims_fp = glob(f'{dir}/*.json')
@@ -32,47 +31,46 @@ def load_vclaims(dir):
         vclaims_list.append(vclaim)
     return vclaims, vclaims_list
 
-def get_bm25(docs):
-    texts = [doc.split() for doc in docs]  # you can do preprocessing as removing stopwords
-    dictionary = corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    bm25_obj = bm25.BM25(corpus)
-    return [bm25_obj, dictionary]
 
-def get_score(iclaim, bm25_title, dictionary_title, bm25_vclaim, dictionary_vclaim, bm25_text, dictionary_text, vclaims_list, index, search_keys, size=10000):
-    iclaims = iclaim.split()
-    query_doc_title = dictionary_title.doc2bow(iclaims)
-    scores_title = bm25_title.get_scores(query_doc_title)
-    query_doc_vclaim = dictionary_vclaim.doc2bow(iclaims)
-    scores_vclaim = bm25_vclaim.get_scores(query_doc_vclaim)
-    query_doc_text = dictionary_text.doc2bow(iclaims)
-    scores_text = bm25_text.get_scores(query_doc_text)
-    scores = [(scores_title[i] + scores_vclaim[i] + scores_text[i])/3 for i in range(len(scores_title))]
-    best_docs = sorted(range(len(scores)), key=lambda i: scores[i])[-size:]
-    score = []
-    for best_doc in best_docs:
-        score.append((vclaims_list[best_doc]['vclaim_id'], scores[best_doc]))
-    return score
+def get_score(iclaim, vclaims_list, index, search_keys, size=10000):
+    query = sbert.encode(iclaim)
+
+    # Compute the encodings for all sentences in a text
+    texts = [vclaim['text'] for vclaim in vclaims_list]
+    encodings_list = [sbert.encode(sent_tokenize(text)) for text in texts]
+
+    # Compute mean scores by calculating cosine similarities between all claims and the query.
+    scores = []
+
+    for encoding in encodings_list:
+        results = util.semantic_search(query, encoding, top_k=5)
+        result_sum = 0
+        for result in results:
+            result_sum += result['score']
+        scores.append((vclaims_list[encodings_list.index(encoding)]['vclaim_id'], result_sum / len(results)))
+
+    return scores
+
 
 def get_scores(iclaims, vclaims_list, index, search_keys, size):
     iclaims_count, vclaims_count = len(iclaims), len(vclaims_list)
-    bm25_title, dictionary_title = get_bm25([vclaim['title'] for vclaim in vclaims_list])
-    bm25_vclaim, dictionary_vclaim = get_bm25([vclaim['vclaim'] for vclaim in vclaims_list])
-    bm25_text, dictionary_text = get_bm25([vclaim['text'] for vclaim in vclaims_list])
     scores = {}
 
     logging.info(f"Geting RM5 scores for {iclaims_count} iclaims and {vclaims_count} vclaims")
+
     for iclaim_id, iclaim in iclaims:
-        score = get_score(iclaim, bm25_title, dictionary_title, bm25_vclaim, dictionary_vclaim, bm25_text, dictionary_text, vclaims_list, index, search_keys=search_keys, size=size)
+        score = get_score(iclaim, vclaims_list, index, search_keys=search_keys, size=size)
         scores[iclaim_id] = score
     return scores
+
 
 def format_scores(scores):
     output_string = ''
     for iclaim_id in scores:
         for i, (vclaim_id, score) in enumerate(scores[iclaim_id]):
-            output_string += f"{iclaim_id}\tQ0\t{vclaim_id}\t{i+1}\t{score}\telasic\n"
+            output_string += f"{iclaim_id}\tQ0\t{vclaim_id}\t{i + 1}\t{score}\telasic\n"
     return output_string
+
 
 def run_baselines(args):
     if not exists('baselines/data'):
@@ -86,23 +84,26 @@ def run_baselines(args):
     for iclaim_id in wanted_iclaim_ids:
         iclaim = all_iclaims.iclaim[all_iclaims.iclaim_id == iclaim_id].iloc[0]
         iclaims.append((iclaim_id, iclaim))
-    
+
     index = f"{args.subtask}-{args.lang}"
 
+    # options are title, vclaim, text
     scores = get_scores(iclaims, vclaims_list, index, search_keys=args.keys, size=args.size)
-    print("her")
-    ngram_baseline_fpath = join(ROOT_DIR, f'baselines/data/subtask_{args.subtask}_bm25-all_{args.lang}_{basename(args.dev_file_path)}')
+    ngram_baseline_fpath = join(ROOT_DIR,
+                                f'baselines/data/subtask_{args.subtask}_bm25_{args.lang}_{basename(args.dev_file_path)}')
     formatted_scores = format_scores(scores)
     with open(ngram_baseline_fpath, 'w') as f:
         f.write(formatted_scores)
     maps, mrr, precisions = evaluate(args.dev_file_path, ngram_baseline_fpath)
-    logging.info(f"Elasticsearch Baseline for Subtask-{args.subtask}--{args.lang}")
+    logging.info(f"S-BERT Baseline for Subtask-{args.subtask}--{args.lang}")
     logging.info(f'All MAP scores on threshold from [1, 3, 5, 10, 20, 50, 1000]. {maps}')
     logging.info(f'MRR score {mrr}')
     logging.info(f'All P scores on threshold from [1, 3, 5, 10, 20, 50, 1000]. {precisions}')
 
-# python baselines/bm25-all.py --train-file-path=baselines/v1/train.tsv --dev-file-path=baselines/v1/train.tsv --vclaims-dir-path=baselines/politifact-vclaims --iclaims-file-path=baselines/v1/iclaims.queries --subtask=2b --lang=english
 
+# python baselines/bm25.py --train-file-path=baselines/v1/train.tsv --dev-file-path=baselines/v1/train.tsv
+# --vclaims-dir-path=baselines/politifact-vclaims --iclaims-file-path=baselines/v1/iclaims.queries --subtask=2b
+# --lang=english
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--train-file-path", "-t", required=True, type=str,
